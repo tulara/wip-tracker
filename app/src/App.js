c900105690100ff5f812ece9fc9916830f4505ed
tulara <tulara@users.noreply.github.com>
2019-12-26 14:27:55 +0000

    adds apollo client and logs api request to console

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import client from './apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
 import './App.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <div className="container">
-        <button className="button is-danger is-outlined" onClick={() => (alert("Hello!"))}>
-         Hello?
-        </button>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <div className="container">
+          <button className="button is-danger is-outlined" onClick={() => (alert("Hello!"))}>
+          Hello?
+          </button>
+        </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 }
 
---
+++ app/src/apollo-client.js
@@ -0,0 +1,46 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+
+import gql from 'graphql-tag';
+
+const endpoint = process.env.API_ENDPOINT || 'http://localhost:4000/';
+
+const httpLink = new HttpLink({
+    uri: endpoint,
+});
+
+const cache = new InMemoryCache();
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.forEach(({ message, locations, path }) =>
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+        ),
+      );
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    errorLink,
+    httpLink
+  ]),
+  cache
+});
+
+client.query({
+    query: gql`
+        query getAllProjects {
+            projects {
+                name
+                recipient
+            }
+        }
+    `
+}).then(result => console.log(result));
+
+export default client;
