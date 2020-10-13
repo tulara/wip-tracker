50ce636c77f764ea6d61037872dde3d63e360955
tulara <tulara@users.noreply.github.com>
2020-10-13 19:40:41 +0100

    new button + formatting

--- app/src/projects/ProjectList.js
+++ app/src/projects/ProjectList.js
@@ -1,45 +1,48 @@
-import React from 'react';
+import React from "react"
 
-import './projects.scss';
-import placeholder from './images/empty-placeholder.jpg';
+import "./projects.scss"
+import placeholder from "./images/empty-placeholder.jpg"
 //import pie from './images/pie.jpg';
 
 const ProjectList = ({ projects, removeProject }) => {
-    return (
-        <div className="columns is-multiline">
-                {projects && projects.map( project => {
-                    console.log(project);
-                    return (
-                    <div 
-                        className="column"
-                        key={project.id}  
-                    >
-                        <div className="card project-list-item">
-                            <div className="card-image project-list-item-image"> 
-                                <img 
-                                    alt="your work in progress"
-                                    className={project.image ? "image" : "image placeholder" }
-                                    src={project.image ? project.image : placeholder} />
-                            </div>
-                            <button
-                                    aria-label="remove project"
-                                    className="delete is-large project-list-item__close"
-                                    onClick={() => { removeProject(project.id) }} />
-                            <div className="card-content">
-                                <h1 className="title is-5 has-text-centered">{project.name}</h1>
-                                <div className="content project-list-item-recipient">
-                                    <span className="icon is-small is-left">
-                                        <i className="fas fa-user"></i>
-                                    </span>
-                                    <p>{project.recipient}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )
-                }
-                )}
-        </div>
-    )
+  return (
+    <div className="columns is-multiline">
+      {projects &&
+        projects.map((project) => {
+          console.log(project)
+          return (
+            <div className="column" key={project.id}>
+              <div className="card project-list-item">
+                <div className="card-image project-list-item-image">
+                  <img
+                    alt="your work in progress"
+                    className={project.image ? "image" : "image placeholder"}
+                    src={project.image ? project.image : placeholder}
+                  />
+                </div>
+                <button
+                  aria-label="remove project"
+                  className="delete is-large project-list-item__close"
+                  onClick={() => {
+                    removeProject(project.id)
+                  }}
+                />
+                <div className="card-content">
+                  <h1 className="title is-5 has-text-centered">
+                    {project.name}
+                  </h1>
+                  <div className="content project-list-item-recipient">
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-user"></i>
+                    </span>
+                    <p>{project.recipient}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+    </div>
+  )
 }
-export default ProjectList;
+export default ProjectList
--- app/src/projects/WIPProjects.js
+++ app/src/projects/WIPProjects.js
@@ -1,92 +1,93 @@
-import React from 'react'
+import React from "react"
 
-import ProjectList from './ProjectList';
-import NewProjectButton from './NewProjectButton';
+import ProjectList from "./ProjectList"
+import NewProjectButton from "./NewProjectButton"
 
-import { useQuery, useMutation } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
+import AddButton from "../components/AddButton"
+
+import { useQuery, useMutation } from "@apollo/react-hooks"
+import gql from "graphql-tag"
 
 const GET_PROJECTS = gql`
-    query getAllProjects {
-        projects {
-            id
-            name
-            recipient
-            image
-        }
+  query getAllProjects {
+    projects {
+      id
+      name
+      recipient
+      image
     }
-`;
+  }
+`
 
 const REMOVE_PROJECT = gql`
-    mutation removeProject($id: String!) {
-        removeProject(id: $id)
-    }
-`;
+  mutation removeProject($id: String!) {
+    removeProject(id: $id)
+  }
+`
 
 const SAVE_NEW_PROJECT = gql`
-    mutation saveProject($name: String!, $recipient: String) {
-      saveProject(name:$name, recipient: $recipient) {
-          id
-          name
-          recipient
-      }
+  mutation saveProject($name: String!, $recipient: String) {
+    saveProject(name: $name, recipient: $recipient) {
+      id
+      name
+      recipient
     }
+  }
 `
 
 const WIPProjects = () => {
-    const { data } = useQuery(GET_PROJECTS)
-    console.log(data)
+  const { data } = useQuery(GET_PROJECTS)
+  console.log(data)
 
-    const [saveProject] = useMutation(SAVE_NEW_PROJECT,
-        {
-            refetchQueries:[{
-                query: GET_PROJECTS,
-            }],
-            update(cache, {data: {saveProject}}) {
-                const { projects } = cache.readQuery({query: GET_PROJECTS});
-                cache.writeQuery({
-                    query: GET_PROJECTS,
-                    data: {projects: projects.concat([saveProject])}
-                });
-            }
-        }
-    );
+  const [saveProject] = useMutation(SAVE_NEW_PROJECT, {
+    refetchQueries: [
+      {
+        query: GET_PROJECTS,
+      },
+    ],
+    update(cache, { data: { saveProject } }) {
+      const { projects } = cache.readQuery({ query: GET_PROJECTS })
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: projects.concat([saveProject]) },
+      })
+    },
+  })
 
-    const [removeProject] = useMutation(REMOVE_PROJECT,
-        {
-            update(cache, {data: {removeProject}}) {
-                console.log(removeProject)
-                const { projects } = cache.readQuery({query: GET_PROJECTS});
-                cache.writeQuery({
-                    query: GET_PROJECTS,
-                    data: { projects: projects.filter((p) => p.id !== removeProject) }
-                })
-            }
-        }
-        
-    )
+  const [removeProject] = useMutation(REMOVE_PROJECT, {
+    update(cache, { data: { removeProject } }) {
+      console.log(removeProject)
+      const { projects } = cache.readQuery({ query: GET_PROJECTS })
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: projects.filter((p) => p.id !== removeProject) },
+      })
+    },
+  })
 
-    const removeProjectById = (id) => {
-        console.log(`removing item with id ${id}`);
-        // stupid apollo structure
-        removeProject({ variables: {id}});
-    }
+  const removeProjectById = (id) => {
+    console.log(`removing item with id ${id}`)
+    // stupid apollo structure
+    removeProject({ variables: { id } })
+  }
 
-    return (
+  return (
     <>
-        <section className="section has-text-centered">
-            <h1 className="title has-text-centered">Projects In Progress</h1>
-            <NewProjectButton saveProject={saveProject} />
-        </section>
-        <section className="section">
-            {data && data.projects &&
-                <ProjectList 
-                    projects={data.projects}
-                    removeProject={removeProjectById} />
-            }
-        </section>
+      <section className="section has-text-centered">
+        <h1 className="title has-text-centered">Stash</h1>
+        <NewProjectButton saveProject={saveProject} />
+        <AddButton onClick={saveProject} />
+      </section>
+      <section className="section">
+        {data && data.projects && (
+          <ProjectList
+            projects={data.projects}
+            removeProject={removeProjectById}
+          />
+        )}
+      </section>
     </>
-    )
+  )
 }
 
-export default WIPProjects;
+export default WIPProjects
